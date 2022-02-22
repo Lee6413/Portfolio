093148c702ba974cd92e81388f84e9b7177a2f14
Jarum Carr <jarumlcarr@gmail.com>
2022-02-21 23:24:39 -0800

    progress is being made

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-// import GitHubIcon from '@material-ui/icons/GitHub'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import EmailIcon from '@material-ui/icons/Email';
 // import LinkedInIcon from '@material-ui/icons/LinkedIn'
 // import { HiOutlineMail } from 'react-icons/fa'
 // import { FaAngellist } from 'react-icons/fa'
@@ -9,7 +10,7 @@ import 'aos/dist/aos.css'
 import './info.css'
 
 const Info = () => {
-  const { name, title, email, phone, city, description, linkedin, github } = info
+  const { name, title, description, resume, contact } = info
 
   useEffect(() => {
     Aos.init({ duration: 1500 })
@@ -22,6 +23,37 @@ const Info = () => {
           Hi, I am <span className='info-name'>{name}.</span>
         </h1>
       )}
+
+      {title && <h2 className='info_title'>A {title}.</h2>}
+
+      {description && <p className='info_desc'>{description}</p>}
+
+      <div classNmae='info_contact center'>
+
+        {resume && (
+          <a href='/Test-Resume.PDF' target='_blank'>
+            <span type='button' className='btn btn-outline'>
+              Resume
+            </span>
+          </a>
+        )}
+
+        {contact && (
+          <>
+            {contact.github && (
+              <a
+                href={contact.github}
+                target='_blank'
+                rel='noreferrer'
+                aria-label='github'
+                className='link link--icon first--link'
+              >
+                <GitHubIcon />
+              </a>
+            )}
+          </>
+        )}
+      </div>
     </div>
   )
 }
--- src/context/appendix.js
+++ src/context/appendix.js
@@ -1,12 +1,15 @@
 const info = {
   name: "Lee Carr",
   title: "Full-Stack Engineer",
-  email: "jarumlcarr@gmail.com",
-  phone: "+1 951 956 9623",
-  city: "Riverside, CA",
   description: "I am a creative problem solver, debugging fan, team player, and effective communicator.",
-  linkedin: "https://www.linkedin.com/in/lee-carr/",
-  github: "https://github.com/Lee6413",
+  resume: 'https://example.com',
+  city: "Riverside, CA",
+  contact: {
+    email: "jarumlcarr@gmail.com",
+    phone: "+1 951 956 9623",
+    linkedin: "https://www.linkedin.com/in/lee-carr/",
+    github: "https://github.com/Lee6413"
+  },
 }
 
 export { info }
